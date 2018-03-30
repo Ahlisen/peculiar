1287566e5e8751508dc8c2b2fab3cbd11c12a0b4
Marcus <ahlisen93@gmail.com>
2018-03-30 12:58:52 +0200

    example video probably working on device

--- App.js
+++ App.js
@@ -5,6 +5,7 @@
  */
 
 import React, { Component } from 'react';
+import Video from 'react-native-video';
 import {
   Platform,
   StyleSheet,
@@ -33,6 +34,29 @@ export default class App extends Component<Props> {
         <Text style={styles.instructions}>
           {instructions}
         </Text>
+        <Video source={require('./data/woman_walk.mp4')}   // Can be a URL or a local file.
+          poster="https://baconmockup.com/300/200/" // uri to an image to display until the video plays
+          ref={(ref) => {
+            this.player = ref
+          }}                                      // Store reference
+          rate={1.0}                              // 0 is paused, 1 is normal.
+          volume={1.0}                            // 0 is muted, 1 is normal.
+          muted={false}                           // Mutes the audio entirely.
+          paused={false}                          // Pauses playback entirely.
+          resizeMode="cover"                      // Fill the whole screen at aspect ratio.*
+          repeat={true}                           // Repeat forever.
+          playInBackground={false}                // Audio continues to play when app entering background.
+          playWhenInactive={false}                // [iOS] Video continues to play when control or notification center are shown.
+          ignoreSilentSwitch={"ignore"}           // [iOS] ignore | obey - When 'ignore', audio will still play with the iOS hard silent switch set to silent.
+          progressUpdateInterval={250.0}          // [iOS] Interval to fire onProgress (default to ~250ms)
+          onLoadStart={this.loadStart}            // Callback when video starts to load
+          onLoad={this.setDuration}               // Callback when video loads
+          onProgress={this.setTime}               // Callback every ~250ms with currentTime
+          onEnd={this.onEnd}                      // Callback when playback finishes
+          onError={this.videoError}               // Callback when video cannot be loaded
+          onBuffer={this.onBuffer}                // Callback when remote video is buffering
+          onTimedMetadata={this.onTimedMetadata}  // Callback when the stream receive some metadata
+          style={styles.video} />
       </View>
     );
   }
@@ -41,7 +65,7 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
@@ -54,5 +78,12 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  video: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   },
 });
