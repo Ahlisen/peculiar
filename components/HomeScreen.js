b90afcca3c80d0835c1f224fbbc14df584030e03
Ahlisen <marcus.ahlstrom@isotop.se>
2018-06-11 23:06:31 +0200

    mvp with placeholder data
    Not integrated with the render step. Vertical scrolling in both input and output view with regret button

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -6,31 +6,96 @@ import {
   View,
   Button,
   FlatList,
-  Image
+  Image,
+  TouchableHighlight,
+  Dimensions
 } from 'react-native';
 
-const thumbnails = [{key: 'a'}, {key: 'b'}];
-
+const {width, height} = Dimensions.get('window');
+const columns = 5
+const itemWidth = width / columns;
 
 class HomeScreen extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    var thumbnails = [];
+
+    for (var i = 0; i < 100; i++) {
+      item = {key: String(i)}
+      thumbnails.push(item)
+    }
+
+    this.state = {
+      output: [],
+      thumbnails: thumbnails
+    };
+  }
+
+  addItem = (item) => {
+    output = this.state.output
+    output.push(item)
+    this.setState({ output })
+  };
+
+  removeLastItem = () => {
+    output = this.state.output
+    if (output.length > 0) {
+      output.pop()
+    }
+    this.setState({ output })
+  };
+
+  renderItem = ({item}) => {
+    return (
+      <View>
+        <Text>key: {item.key}</Text>
+        <Image style={{ height: itemWidth,  width : itemWidth}} source={require('../images/placeholder2.png')} resizeMode='cover' />
+      </View>
+    )
+  };
+
+  renderInputItem = ({item}) => {
+    return (
+      <TouchableHighlight onPress={() => this.addItem(item)}>
+        <Image style={{ height: itemWidth,  width : itemWidth}} source={require('../images/placeholder2.png')} resizeMode='cover' />
+      </TouchableHighlight>
+    )
+  };
+
+  /*
+  *   loading assets in ios: { uri: 'check' }
+  *   require() loads noticably slower than {uri:_} in flatlist.
+  *   
+  */
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Instructions"
-          onPress={() => this.props.navigation.navigate('Instructions')}
-        />
-        <Button
-          title="Render"
-          onPress={() => this.props.navigation.navigate('Result')}
-        />
-        <Image source={{uri: 'check'}} style={{width: 42, height: 42}} />
-        <FlatList
-          horizontal
-          data={thumbnails}
-          renderItem={({item}) => <Text>{item.key}</Text>}
-        />
+
+        <View style={styles.output}>
+          <FlatList 
+            extraData={this.state}
+            numColumns={columns}
+            data={this.state.output}
+            renderItem={this.renderItem}
+          />
+          <TouchableHighlight style={styles.bottomRight} onPress={() => this.props.navigation.navigate('Result')}>
+            <Image style={{ height: itemWidth,  width : itemWidth}} source={require('../images/render.png')} resizeMode='cover' />
+          </TouchableHighlight>
+        </View>
+        <View style={styles.input}>
+          <FlatList 
+            // horizontal
+            numColumns={columns}
+            data={this.state.thumbnails}
+            renderItem={this.renderInputItem}
+          />
+          <TouchableHighlight style={styles.bottomRight} onPress={() => this.removeLastItem()}>
+            <Image style={{ height: itemWidth,  width : itemWidth}} source={require('../images/remove.png')} resizeMode='cover' />
+          </TouchableHighlight>
+        </View>
       </View>
     );
   }
@@ -41,8 +106,22 @@ type Props = {};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: 'flex-start',
     justifyContent: 'center'
+  },
+  output: {
+    position: 'relative',
+    width: width,
+    height: itemWidth * 4
+  },
+  input: {
+    position: 'relative',
+    height: itemWidth * 4
+  },
+  bottomRight: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0
   }
 });
 
