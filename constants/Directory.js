b48c1ee356a9dc77a39a270cdfbf10268334e72a
Fredrik Berglund <fredrik.911225@gmail.com>
2018-06-15 17:49:09 +0200

    Fix Directory

--- constants/Directory.js
+++ constants/Directory.js
@@ -1,31 +1,25 @@
-import Platform from "react-native";
+import { Platform } from "react-native";
 import RNFS from "react-native-fs";
 
-// export default class Directory {
-//     static get VIDEO() {
-//         return Platform.select({
-//             ios: () => RNFS.DocumentDirectoryPath + "/videos/",
-//             android: () => RNFS.DocumentDirectoryPath + "/videos/"
-//         })();
-//     }
+export default class Directory {
+    static get VIDEO() {
+        return Platform.select({
+            ios: () => RNFS.DocumentDirectoryPath + "/videos/",
+            android: () => RNFS.DocumentDirectoryPath + "/videos/"
+        })();
+    }
 
-//     static get ICON() {
-//         return Platform.select({
-//             ios: () => RNFS.DocumentDirectoryPath + "/icons/",
-//             android: () => RNFS.DocumentDirectoryPath + "/icons/"
-//         })();
-//     }
+    static get ICON() {
+        return Platform.select({
+            ios: () => RNFS.DocumentDirectoryPath + "/icons/",
+            android: () => RNFS.DocumentDirectoryPath + "/icons/"
+        })();
+    }
 
-//     static get PICTOGRAM() {
-//         return Platform.select({
-//             ios: () => RNFS.DocumentDirectoryPath + "/pictograms/",
-//             android: () => RNFS.DocumentDirectoryPath + "/pictograms/"
-//         })();
-//     }
-// }
-
-export default {
-    VIDEO: RNFS.DocumentDirectoryPath + "/videos/",
-    ICON: RNFS.DocumentDirectoryPath + "/icons/",
-    PICTOGRAM: RNFS.DocumentDirectoryPath + "/pictograms/"
+    static get PICTOGRAM() {
+        return Platform.select({
+            ios: () => RNFS.DocumentDirectoryPath + "/pictograms/",
+            android: () => RNFS.DocumentDirectoryPath + "/pictograms/"
+        })();
+    }
 }
